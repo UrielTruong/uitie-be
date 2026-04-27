4cdac8a60e0e7e373dd4bc6f9de32c8c6abefbcb
HuynhTruong <uri.tr157@gmail.com>
2026-04-27 18:58:29 +0700

    feat: middleware authen

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,21 @@
 
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\AuthenticatedController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function (): void {
     Route::get('login', [AuthenticatedController::class, 'login'])
         ->name('login');
+});
+
+
+Route::middleware('auth.jwt')->group(function () {
+    Route::get('test', function (Request $request) {
+        return response()->json([
+            'message' => 'Hello World',
+            'userId' => $request->user_id,
+            'userRole' => $request->user_role,
+        ]);
+    });
 });
